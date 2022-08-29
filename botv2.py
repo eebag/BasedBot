06af0065189548ecad6c3703edc443f0a85df45d
eebag <gabehohman@hotmail.com>
2022-08-29 15:19:28 -0600

    Worked on assinging roles and reading users from mentions

--- botv2.py
+++ botv2.py
@@ -2,6 +2,9 @@
 import json
 from datetime import date
 import discord
+from discord import Member
+from discord.ext.commands import has_permissions
+from discord.utils import get
 from discord.ext import commands
 import csv
 import os
@@ -22,6 +25,7 @@ with open(tokenfile) as t:
 
 # Social credit dict {userid -> social credit}
 socialCredit = {}
+roles = ["1013907404460662786"]
 
 ###########################################################
 # Helper/Misc Functions
@@ -38,25 +42,53 @@ async def findUser(target, ctx):
 
     return False
 
+
 # Admin help command
-@bot.command
-@commands.has_permissions(admin=True)
+@bot.command()
 async def commands(ctx, *args):
-    if len(args == 0):
+    if len(args) == 0:
         await ctx.send("Social Credit commands: \n"
                        "add [user] [amount] -> adds [amount] social credit to [user]'s account\n"
                        "sub [user] [amount] -> subtracts [amount] from [user]'s social credit\n"
                        "bankroll            -> prints out EVERYONE'S social score")
                        # "[user] -> displays user's current social credit, and how much they've gained today")
 
+
 @bot.command(name="add")
-@commands.has_permossions(admin=True)
-async def add_social_credit(ctx, amount, user: discord.user = None)
+async def add_social_credit(ctx, amount, user: discord.user = None):
     if not user:
         await ctx.send("Invalid user or self mention")
     else:
         await ctx.send(user.mention)
 
+
+@bot.command()
+@has_permissions(administrator = True)
+async def giverole(ctx, user : discord.Member, *, role : discord.role = None):
+
+    if role in user.roles:
+        await ctx.send(f"{user.mention} already has the role {role}!")
+    else:
+        await user.add_roles(role)
+        await ctx.send(f"{role} added to {user.mention} !!")
+
+
+@giverole.error
+async def role_error(self, ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("Nice try, pal")
+
+
+@bot.command(name="setmaxrole")
+@has_permissions(administrator = True)
+async def set_max_role(ctx, *, role : discord.role):
+    await ctx.send(f"{role} now set as max role")
+
+
+@bot.command(name="addrole")
+@has_permissions(administrator = True)
+async def add_role(ctx, *, role : discord.role):
+    await ctx.send(f"{role} now added")
 
 
 # non-admin commands
@@ -68,13 +100,13 @@ async def check_social_credit(ctx, *args):
 # Setup/initialization commands
 
 # init: use when setting up bot for server.  Also usable as reset.
-@bot.command
+@bot.command()
 async def init(ctx, *args):
     pass
 
 
 # setup: use after bot restart
-@bot.command
+@bot.command()
 async def setup(ctx, *args):
     pass
 
@@ -85,3 +117,5 @@ async def on_ready():
     # print("Loading Social Credit csv")
     # loadSocialCredit(SCFile)
     print("Ready!")
+
+bot.run(token)
