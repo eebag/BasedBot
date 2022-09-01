18aa6bb52edfd5b635bec6e6eb68ed993a06787b
eebag <gabehohman@hotmail.com>
2022-08-31 20:56:55 -0600

    added save/load rank methods

--- save_load_module.py
+++ save_load_module.py
@@ -1,10 +1,4 @@
 import csv
-import discord
-from discord import Member
-from discord.ext import commands
-from discord.ext.commands import has_permissions
-from discord.utils import get
-import csv
 
 ###################
 # module for saving and loading information
@@ -19,12 +13,46 @@ def save_user_data(filename:str, data:dict):
         for key in data.keys():
             newDict = {'UserID': key, 'Points': data[key]}
             writer.writerow(newDict)
-    pass
 
 def load_user_data(filename:str):
-    pass
+    data = {}
+
+    with open(filename, 'r') as f:
+        for line in csv.DictReader(f):
+            userid = line['UserID']
+            points = line['Points']
+
+            data[int(userid)] = int(points)
+
+    return data
 
 
 # test
-dict = {123:1, 234:2}
-save_user_data("testfile.csv", dict)
+# dict = {123:1, 234:2}
+# save_user_data("testfile.csv", dict)
+#
+# test = load_user_data("testfile.csv")
+# print(test)
+
+###################
+# server rank settings save/load
+def save_rank_settings(filename: str, ranks: dict):
+    fields = ['Points', 'Rank']
+    with open(filename, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+        for key in ranks.keys():
+            newDict = {'Points': key, 'Rank': ranks[key]}
+            writer.writerow(newDict)
+
+def load_rank_settings(filename: str, settings: dict):
+    ranks = {}
+
+    with open(filename, 'r') as f:
+        for line in csv.DictReader(f):
+            points = line['UserID']
+            rank = line['rank']
+
+            ranks[int(points)] = rank
+
+    return ranks
